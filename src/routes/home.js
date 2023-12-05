ab93f6e8176fa281ee087ac3522297d370d7bc60
ella00100 <ella00100@github.com>
2023-12-05 19:18:29 +0900

    feat : manager page - select func

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,10 +1,9 @@
 import express from 'express';
-import { insertSql } from '../database/sql';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('home', { data: " " });
+    res.render('home');
 })
 
 module.exports = router;
--- src/routes/manager.js
+++ src/routes/manager.js
@@ -1,9 +1,96 @@
+// manager.js
+
 import express from 'express';
+import { selectSql, insertSql, updateSql, deleteSql } from '../database/sql';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
-})
+router.get('/', async (req, res) => {
+    if (!req.session.user || req.session.user.role !== 'Manager') {
+        return res.redirect('/');
+    }
 
-module.exports = router;
+    try {
+        const doctor = await selectSql.getDoctor();
+        const nurse = await selectSql.getNurse();
+
+        res.render('manager', {
+            title: 'Medicalist',
+            title1: 'Doctor',
+            title2: 'Nurse',
+            doctor,
+            nurse,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/addDoctor', async (req, res) => {
+    try {
+        const data = req.body;
+        await insertSql.setDoctor(data);
+        res.redirect('/manager');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/addNurse', async (req, res) => {
+    try {
+        const data = req.body;
+        await insertSql.setNurse(data);
+        res.redirect('/manager');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/updateDoctor', async (req, res) => {
+    try {
+        const data = req.body;
+        await updateSql.updateDoctor(data);
+        res.redirect('/manager');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/updateNurse', async (req, res) => {
+    try {
+        const data = req.body;
+        await updateSql.updateNurse(data);
+        res.redirect('/manager');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/deleteDoctor', async (req, res) => {
+    try {
+        const data = req.body;
+        await deleteSql.deleteDoctor(data);
+        res.redirect('/manager');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.post('/deleteNurse', async (req, res) => {
+    try {
+        const data = req.body;
+        await deleteSql.deleteNurse(data);
+        res.redirect('/manager');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+export default router;
